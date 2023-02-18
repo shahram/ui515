4ab01e94712f01a08eb01d3be65ee04f74db467e
shahram <shahram.shahdadi@gmail.com>
2023-02-18 01:34:43 +0100

    Initial commit ui505

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
+   "use strict"
+   return Controller.extend("de.jlabs.demo.controller.App", {
+      incrementBy1: function () {
+         let myTextElem = this.getView().byId("counter")
+         let myNum = parseInt(myTextElem.getText())
+         let myNewNum = myNum + 1
+         myTextElem.setText(myNewNum)
+      },
+      onInit: function () {
+         let oModel = new JSONModel(
+            sap.ui.require.toUrl("de/jlabs/demo/Data.json")
+         )
+         this.getView().setModel(oModel)
+      }
+   })
+})
